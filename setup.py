57bc066f99e20ef582fcb66bafd82ac7d41b3712
Pedro Ortiz <pedro.ortiz@unifin.com.mx>
2024-02-05 15:19:12 -0600

    Agregando una estructura cookiecutter al proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple machine learning model to detect spam from several entries of emails. The dataset its from Kaggel',
+    author='Pedro Ortiz',
+    license='MIT',
+)
